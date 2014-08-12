57efff4db201eba2de5869facd6a250d3d716a88
Tianqi Tong <tianqitong@outlook.com>
2014-08-12 16:31:38 -0400

    modified sample file for import

--- src/main/java/controller/SystemController.java
+++ src/main/java/controller/SystemController.java
@@ -12,25 +12,21 @@ public class SystemController {
 
     public static final String USER_CSV_PATH = "/researcher/allcsv";
     public static final String DEVICE_CSV_PATH = "/devices/allcsv";
-    public static final String PROTOCOL_CSV_PATH = "/protocols/allcsv";
 
     public static final String USER_IMPORT_PATH = "/researcher/importcsv";
     public static final String DEVICE_IMPORT_PATH = "/devices/importcsv";
-    public static final String PROTOCOL_IMPORT_PATH = "/protocols/importcsv";
 
-    public static final String USER_SAMPLE_PATH = "/sample/researchers.csv";
+    public static final String USER_SAMPLE_PATH = "/sample/users.csv";
     public static final String DEVICE_SAMPLE_PATH = "/sample/devices.csv";
-    public static final String PROTOCOL_SAMPLE_PATH = "/sample/protocols.csv";
 
     @RequestMapping(method = RequestMethod.GET)
     public String render(ModelMap model) {
         model.addAttribute("user_csv_path", USER_CSV_PATH);
         model.addAttribute("device_csv_path", DEVICE_CSV_PATH);
-        model.addAttribute("protocol_csv_path", PROTOCOL_CSV_PATH);
 
         model.addAttribute("user_sample_path", USER_SAMPLE_PATH);
         model.addAttribute("device_sample_path", DEVICE_SAMPLE_PATH);
-        model.addAttribute("protocol_sample_path", PROTOCOL_SAMPLE_PATH);
+
         return "system";
     }
 
@@ -47,9 +43,6 @@ public class SystemController {
             title = "Device";
             uploadUrl = DEVICE_IMPORT_PATH;
 
-        } else if (cate.equals("protocol")) {
-            title = "Protocol";
-            uploadUrl = PROTOCOL_IMPORT_PATH;
         }
 
         model.addAttribute("title", title);
